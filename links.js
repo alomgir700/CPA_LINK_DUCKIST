18889187ee8a0037effe4c625c0804ac6052d903
alomgir700 <mdalomgirhossen7578@gmail.com>
2024-09-09 19:09:45 +0600

    Update links.js

--- links.js
+++ links.js
@@ -1,4 +1,4 @@
-  var gplink = 'https://aff.mehedibhai.shop/?utm_medium=2a2b2221b2745fcb37870e78896867dd00e8876a&utm_campaign=linkgp';
-  var bllink = 'https://aff.mehedibhai.shop/?utm_medium=d7e5e10b459e84c7b6aa1befb66b61ffc5c7ad5c&utm_campaign=linkbl';
-  var arlink = 'https://aff.mehedibhai.shop/?utm_medium=c233621a6507063592d39e75d1d9293fc9b87596&utm_campaign=linkartel';
-  var rblink = 'https://aff.mehedibhai.shop/?utm_medium=c233621a6507063592d39e75d1d9293fc9b87596&utm_campaign=linkartel';
+  var gplink = 'https://msm.mahedims.xyz/?utm_medium=8b261cc63d71ed7d620a0e904c78d13cef8dc172&utm_campaign=gggp';
+  var bllink = 'https://msm.mahedims.xyz/?utm_medium=da1992b35ad730ca7d0fa79cdb79c09da89d93a1&utm_campaign=bbbl';
+  var arlink = 'https://msm.mahedims.xyz/?utm_medium=203a9d391ddb936a84b7c420f1edba5a1a10c946&utm_campaign=aaar';
+  var rblink = 'https://msm.mahedims.xyz/?utm_medium=203a9d391ddb936a84b7c420f1edba5a1a10c946&utm_campaign=aaar';
