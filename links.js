6bff197c0f1fc06363ecf27eda54cedb32f6c955
alomgir700 <mdalomgirhossen7578@gmail.com>
2024-09-11 15:22:32 +0600

    Update links.js

--- links.js
+++ links.js
@@ -1,4 +1,4 @@
-  var gplink = 'https://msm.mahedims.xyz/?utm_medium=1062f0b30e37b2bc5ff89cbd48c9c3d4e351d6d3&utm_campaign=gggg11';
-  var bllink = 'https://msm.mahedims.xyz/?utm_medium=f5df29cd3637b1adc2dece552c02a75fd8d1cec6&utm_campaign=bbb11';
-  var arlink = 'https://msm.mahedims.xyz/?utm_medium=879f63da4b267490e8d030ed60ca28409e4a8efc&utm_campaign=aaaa111';
-  var rblink = 'https://msm.mahedims.xyz/?utm_medium=879f63da4b267490e8d030ed60ca28409e4a8efc&utm_campaign=aaaa111';
+  var gplink = 'https://smrturl.co/o/s10fc1ff592/53489651?s1=';
+  var bllink = 'https://smrturl.co/o/s10fc1ff592/53385239?s1=';
+  var arlink = 'https://smrturl.co/o/s10fc1ff592/53385239?s1=';
+  var rblink = 'https://smrturl.co/o/s10fc1ff592/53385239?s1=';
