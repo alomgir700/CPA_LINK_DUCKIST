69c6c43a93c3501702fb6bf5d8cb73feed828da7
alomgir700 <mdalomgirhossen7578@gmail.com>
2024-11-19 12:07:26 +0600

    Update links.js

--- links.js
+++ links.js
@@ -1,4 +1,4 @@
-  var gplink = 'https://smrturl.co/a/s77e732a8e9/4813?s1=';
-  var bllink = 'https://smrturl.co/a/s77e732a8e9/4813?s1=';
+  var gplink = 'https://aff.mehedibhai.shop/?utm_medium=ade09b377b92ebb556c597c214bba542f1f2f0fd&utm_campaign=gpblrobi';
+  var bllink = 'https://aff.mehedibhai.shop/?utm_medium=ade09b377b92ebb556c597c214bba542f1f2f0fd&utm_campaign=gpblrobi';
   var arlink = 'https://smrturl.co/a/s77e732a8e9/4813?s1=';
-  var rblink = 'https://smrturl.co/a/s77e732a8e9/4813?s1=';
+  var rblink = 'https://aff.mehedibhai.shop/?utm_medium=ade09b377b92ebb556c597c214bba542f1f2f0fd&utm_campaign=gpblrobi';
