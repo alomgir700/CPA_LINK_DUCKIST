dcc73ee46c54e7a48c0c23292acb36afcfa5e901
alomgir700 <mdalomgirhossen7578@gmail.com>
2024-11-21 13:25:25 +0600

    Update links.js

--- links.js
+++ links.js
@@ -1,4 +1,4 @@
-  var gplink = 'https://smrturl.co/a/s77e732a8e9/4813?s1=';
-  var bllink = 'https://smrturl.co/a/s77e732a8e9/4813?s1=';
+  var gplink = 'https://jk.jibonjs.xyz/?utm_medium=2b189adc6b2ce59f503c1f7856b062cf1b25d863&utm_campaign=Jm';
+  var bllink = 'https://jk.jibonjs.xyz/?utm_medium=b9da5d1f21ecbcab191e15b495c875cf5661005e&utm_campaign=Jm';
   var arlink = 'https://smrturl.co/a/s77e732a8e9/4813?s1=';
-  var rblink = 'https://smrturl.co/a/s77e732a8e9/4813?s1=';
+  var rblink = 'https://jk.jibonjs.xyz/?utm_medium=c1e27d0f0ff2fe175910a20fa48d13fa7dcab9e3&utm_campaign=Jm';
