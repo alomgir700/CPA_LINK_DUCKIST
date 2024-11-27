c8c84746991f703b5a19e5cf91df8b0c284afccb
alomgir700 <mdalomgirhossen7578@gmail.com>
2024-11-27 07:47:42 -0800

    Update links.js

--- links.js
+++ links.js
@@ -1,4 +1,4 @@
-  var gplink = 'https://jk.jibonjs.xyz/?utm_medium=2b189adc6b2ce59f503c1f7856b062cf1b25d863&utm_campaign=Jm';
-  var bllink = 'https://jk.jibonjs.xyz/?utm_medium=b9da5d1f21ecbcab191e15b495c875cf5661005e&utm_campaign=Jm';
+  var gplink = 'https://anu.jibonjs.xyz/?utm_medium=feca3de9ff8f9e8cee09a2c1d754271e241ea52b&utm_campaign=mehedigp1';
+  var bllink = 'https://anu.jibonjs.xyz/?utm_medium=5674266e475c5b86553c2999fa9ca08bf71c6fc1&utm_campaign=mehedibl1';
   var arlink = 'https://smrturl.co/a/s77e732a8e9/4813?s1=';
-  var rblink = 'https://jk.jibonjs.xyz/?utm_medium=c1e27d0f0ff2fe175910a20fa48d13fa7dcab9e3&utm_campaign=Jm';
+  var rblink = 'https://anu.jibonjs.xyz/?utm_medium=5e46bb7bcd85b9a8d8afa8767a51036c8eb3b3e8&utm_campaign=mehedirobi';
