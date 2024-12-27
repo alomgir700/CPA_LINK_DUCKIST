b0c34ebae5ec79c4c8bcd70eff4cd47d8d90f947
alomgir700 <mdalomgirhossen7578@gmail.com>
2024-12-26 19:39:39 -0800

    Update links.js

--- links.js
+++ links.js
@@ -1,4 +1,4 @@
   var gplink = 'https://qjwacdf.com/cl/bd5c4698912c6f0b?p1=&p2=&source=mehedi&site=gp';
   var bllink = 'https://qjwacdf.com/cl/ece3bf0fd76d50b2?p1=&p2=&source=mehedi&site=bl';
-  var arlink = 'https://smrturl.co/a/s77e732a8e9/4813?s1=';
+  var arlink = 'https://qjwacdf.com/cl/43707767e27cc65c?p1=&p2=&source=mehedi&site=artel';
   var rblink = 'https://qjwacdf.com/cl/0107daf0cc8984bf?p1=&p2=&source=mehedi&site=robi';
