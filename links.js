d4def10139b9dc4ec0d29d0b3087e17798bc10eb
alomgir700 <mdalomgirhossen7578@gmail.com>
2025-01-06 07:10:37 -0800

    Update links.js

--- links.js
+++ links.js
@@ -1,4 +1,4 @@
-  var gplink = 'https://qjwacdf.com/cl/bd5c4698912c6f0b?p1=&p2=&source=mehedi&site=gp';
-  var bllink = 'https://qjwacdf.com/cl/ece3bf0fd76d50b2?p1=&p2=&source=mehedi&site=bl';
-  var arlink = 'https://qjwacdf.com/cl/43707767e27cc65c?p1=&p2=&source=mehedi&site=artel';
-  var rblink = 'https://qjwacdf.com/cl/0107daf0cc8984bf?p1=&p2=&source=mehedi&site=robi';
+  var gplink = 'https://aff.foodgass.xyz/?utm_medium=6a0ca6708def6fdda9f332ce6125f3905e312b2f&utm_campaign=nibir10gp';
+  var bllink = 'https://aff.foodgass.xyz/?utm_medium=ff0a375e162996251866587c2ea081992b5da3a0&utm_campaign=nibir10bl';
+  var arlink = 'https://aff.foodgass.xyz/?utm_medium=30dafe84e3f52f396ac9bb4aa88121649098d0e7&utm_campaign=nibir10robi';
+  var rblink = 'https://aff.foodgass.xyz/?utm_medium=30dafe84e3f52f396ac9bb4aa88121649098d0e7&utm_campaign=nibir10robi';
